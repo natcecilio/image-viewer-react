179d9fa80af0aa11f956d788e613b3f92680cfed
natcecilio <tatafly2009@hotmail.com>
2020-12-10 17:48:23 -0300

    primeiro commit teste viewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {RViewerTrigger, RViewer} from 'react-viewerjs';
 
 function App() {
+
+  let images = [
+    "./img/2.jpg",
+    "./img/ah-3.jpg",
+    "./img/ak-4.jpg",
+    "./img/ev-1.jpg",
+    "./img/ka-5.jpg",
+    "./img/se-6.jpg",
+    "./img/evelynn.jpg"
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <RViewer imageUrls={images}>
+      <div style={{display: 'flex', marginTop: '40px'}}>
+        {images.map((image, index)=>{
+          return(
+            <ul  key={index}>
+              <RViewerTrigger index={index}>
+                <img src={image} style={{width: '150px', height: '150px', marginLeft: '20px', border: '2px solid black' }} />
+              </RViewerTrigger>
+            </ul>
+          )
+        })}
+      </div>
+     </RViewer>
     </div>
   );
 }
